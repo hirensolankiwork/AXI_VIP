b77f3e4753045e29823b7180896dec5926a331ec
Aditya Mishra <140566035+adit0131@users.noreply.github.com>
2023-07-26 11:47:45 +0530

    not needed

--- SIM/file_open.bat
+++
@@ -1,7 +0,0 @@
-@ECHO ON
-echo "Opening the AXI Files."
-start gvim -p ..\RTL\*.sv ..\ENV\*.sv ..\TEST\*.sv ..\TOP\*.sv 
-call gvim -p ..\ENV\MAS_AXI_ENV\*.sv 
-call gvim -p ..\ENV\SLV_AXI_ENV\*.sv 
-call gvim -p ..\SIM\*.do ..\SIM\*.log
-exit
